d14ddfd9e79f39f31d2c3e101a12fd7615dce21a
Kiki Dolan <cdolan323@gmail.com>
2018-08-09 23:42:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 /* Enter the code to remove the main node element under this comment */
 
+main.remove();
+
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+
+var element = document.createElement("h1");
+var body = document.getelementbyid("body");
+body.appendchild(element);
+var text = document.createTextNode("Kiki is the champion!");
+element.appendchild(text);
+
+
+
+
+
+
+
+
+
+
